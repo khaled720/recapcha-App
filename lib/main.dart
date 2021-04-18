9cb456006f07919fff62c64e7787199a3d121688
khaled720 <k900000001@gmail.com>
2021-04-18 22:12:27 +0200

    removeSecret

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -41,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -59,14 +34,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   RecaptchaV2Controller recaptchaV2Controller = RecaptchaV2Controller();
-String token=""; 
+  String token = "";
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -76,70 +46,60 @@ String token="";
     // TODO: implement initState
     super.initState();
     recaptchaV2Controller.show();
-
   }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-    
-        child: recaptchaV2Controller.visible?
-        RecaptchaV2(
+          child: recaptchaV2Controller.visible
+              ? RecaptchaV2(
+                  apiKey:
+                      "6LdyO6oaAAAAANlrysO6wlAgVmzwMnrHywmjfwqc", // for enabling the reCaptcha
+                  controller: recaptchaV2Controller,
+                  onToken: (txt) {},
 
-             apiKey: "6LdyO6oaAAAAANlrysO6wlAgVmzwMnrHywmjfwqc", // for enabling the reCaptcha
-          apiSecret: "6LdyO6oaAAAAAOFh4HoDwM3-3BLsiiwKEe6LAl_M", // for verifying the responded token
-          controller: recaptchaV2Controller,
-onToken: (txt){},
-          
-          onVerifiedError: (err){
+                  onVerifiedError: (err) {
+                    print("%%%%%%%%%%%%%%%%%>");
+                    print(err);
+                    Toast.show("Capcha Error !!", context);
+                  },
 
-            print("%%%%%%%%%%%%%%%%%>");
-            print(err);
-            Toast.show("Capcha Error !!", context);
+                  onVerifiedSuccessfully: (success) {
+                    setState(() {
+                      if (success) {
+                        print(
+                            "##############################################################");
+                        Toast.show("Capcha Success", context);
 
-          },
-          
-          onVerifiedSuccessfully: (success) {
-            setState(() {
-              if (success) {
-                print("##############################################################");
-                Toast.show("Capcha Success", context);
+                        /*  token is saved in responseToken variable you can send it now to backend */
 
-            /*  token is saved in responseToken variable you can send it now to backend */
-              
-              print("Response Token: "+recaptchaV2Controller.responseToken.toString());
-                // You've been verified successfully.
-                // 
-             setState(() {
-                 token=recaptchaV2Controller.responseToken.toString();
-              
-             });
-              } else {
-                print("************************************************************");
-                Toast.show("Failed to verify", context);
-                // "Failed to verify.
-              }
-            });
-          },
-        )
-   
-   :Padding(
-     padding: const EdgeInsets.all(10.0),
-     child: Text("Your Token is : "+token),
-   )
-      ),
-   
+                        print("Response Token: " +
+                            recaptchaV2Controller.responseToken.toString());
+                        // You've been verified successfully.
+                        //
+                        setState(() {
+                          token =
+                              recaptchaV2Controller.responseToken.toString();
+                        });
+                        recaptchaV2Controller.hide();
+                      } else {
+                        print(
+                            "************************************************************");
+                        Toast.show("Failed to verify", context);
+                        // "Failed to verify.
+                      }
+                    });
+                  },
+                )
+              : Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text("Your Token is : " + token),
+                )),
+
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
